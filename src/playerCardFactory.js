6a40dc36c08b44fd2d28dcf735604937d0a0ede6
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-08-20 15:46:25 -0400

    Add playerCardFactory
    Add new create player card function: makes a div
    for player cards based on a Player instance:
    - player tag (name and player type) text
    - dataset.name to identify Player instance
    - board div, based on Player's GameBoard inside
    
    Style playerCards with CSS

---
+++ src/playerCardFactory.js
@@ -0,0 +1,21 @@
+import createBoardUI from "./boardUIFactory";
+
+export default function createPlayerCard (player){
+    // player is a Player instance to build the playerCard
+
+    const card = document.createElement("div");
+
+    card.dataset.name = player.name;
+    
+    const playerTag = document.createElement("p");
+    const playerType = player.playerType === "real" ? "real" : "CPU"
+    playerTag.textContent = `Admiral: ${player.name} (${playerType})`;
+
+    // Use player.board to build board in the DOM
+    const playerBoard = createBoardUI(player.board);
+
+    [ playerTag, playerBoard ]
+    .forEach((element) => card.append(element));
+
+    return card;
+}
