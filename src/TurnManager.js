6c63170cf9ef5632712298162accb194c0486590
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-08-31 17:57:29 -0400

    Add extra functions to TurnManager

--- src/TurnManager.js
+++ src/TurnManager.js
@@ -7,14 +7,30 @@ export default class TurnManager {
         return this.currentTurn;
     }
 
+    static getNextTurn(){
+        return (this.currentTurn + 1) % playerList.length;
+    }
+
     static getCurrentPlayerName(){
         return playerList[this.currentTurn] ? 
             playerList[this.currentTurn].name :
             "N/A";
     }
 
+    static getCurrentPlayer(){
+        return playerList[this.currentTurn] ? 
+            playerList[this.currentTurn] :
+            null;
+    }
+
+    static getNextPlayerName(){
+        return playerList[this.getNextTurn()] ?
+            playerList[this.getNextTurn()].name :
+            null;
+    }
+
     static nextTurn(){
-        this.currentTurn = (this.currentTurn + 1) % playerList.length;
+        this.currentTurn = this.getNextTurn();
     }
 
     static resetTurn(){
