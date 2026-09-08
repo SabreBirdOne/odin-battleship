5ed96019bbd8251cb5a7e83a9c07f24cdefffd7d
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-09-08 13:03:55 -0400

    Fix main.js and index.js
    These files have been used throughout GUI
    development. This commit is to round out the
    webpage as part of the UI branch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import init from "./main.js"
-// import css files here
 
+// import css files here
 import "./squareButton.css"
 import "./boardUI.css"
 import "./playerCards.css"
+import "./playerDisplay.css"
+import "./placeShipForm.css"
 
 
 init();
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import playerList from "./playerList";
 import Player from "./Player";
 
 import createPlayerCard from "./playerCardFactory";
+import { getPlayer } from "./playerLookup";
+import updateBoardUI from "./updateBoardUI";
+import createCurrentPlayerDisplay from "./currentPlayerDisplayFactory";
+import updateCurrentPlayerDisplay from "./updateCurrentPlayerDisplay";
+import createWinnerDisplay from "./winnerDisplayFactory";
 
 export default function init(){
     const body = document.querySelector("body");
@@ -14,15 +19,28 @@ export default function init(){
 
     playerList.push(new Player("Admiral 1"));
     playerList.push(new Player("ABYSS", "computer"));
+
+    // Place ships to each player's board
+    const admiral1 = getPlayer("Admiral 1");
+    const abyss = getPlayer("ABYSS");
     
     for (const player of playerList){
         // Add playerCards to this div based on playerList
-        playerCards.appendChild(createPlayerCard(player));
+        let playerCard = createPlayerCard(player);
+        playerCards.appendChild(playerCard);
+
+        updateBoardUI(player.board, playerCard.querySelector(".boardUI"))
     }
 
-    
+    const currentPlayerDisplay = createCurrentPlayerDisplay();
+    updateCurrentPlayerDisplay(currentPlayerDisplay);
+
+    const winnerDisplay = createWinnerDisplay();
+
     [
         mainPageHeader,
-        playerCards
+        currentPlayerDisplay,
+        winnerDisplay,
+        playerCards,
     ].forEach((element) => body.appendChild(element));
 }
